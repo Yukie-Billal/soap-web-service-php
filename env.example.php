04dcb4ab7cf85539bce9b49aa1d688a66fcdcb29
Yukie M Billal <yukiembillal01@gmail.com>
2024-05-02 16:23:14 +0700

    feat: todos service

---
+++ env.example.php
@@ -0,0 +1,8 @@
+<?php
+
+class Database {
+   protected $DBHOST = "localhost";
+   protected $DBUSER = "root";
+   protected $DBPASS = "";
+   protected $DBNAME = "todos-api";
+}
---
+++ todos.php
@@ -0,0 +1,32 @@
+<?php
+include_once 'env.php';
+
+class Todos extends Database {
+   protected $connection;
+   
+   public function all() {
+      $connection = mysqli_connect($this->DBHOST, $this->DBUSER, $this->DBPASS, $this->DBNAME);
+      $results = mysqli_query($connection, "SELECT * FROM todos");
+      
+      $data = [];
+      while ($result = $results->fetch_assoc()) {
+         $data[] = $result;
+      }
+      return json_encode($data);
+      $connection->close();
+   }
+   
+   public function byId($id) {
+      $connection = mysqli_connect($this->DBHOST, $this->DBUSER, $this->DBPASS, $this->DBNAME);
+      $result = mysqli_query($connection, "SELECT * FROM todos WHERE id ='$id'");
+      $connection->close();
+      return json_encode($result);
+   }
+}
+
+$server = new SoapServer(null, array('uri' => "http://soap-web-service.test/todos.php"));
+
+$server->setClass('Todos');
+
+$server->handle();
+
